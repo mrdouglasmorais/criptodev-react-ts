d34e1b4ad3f88d8eb16c9aea6fbb030626e3e0be
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-04-02 09:33:25 -0300

    create components and views

---
+++ src/Components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return(
+    <footer>
+      <div>
+        <p>Todos os direitos reservados</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/Components/Nav/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav: React.FC = () => {
+  return (
+    <div>
+      <p>Itens navegaveis</p>
+    </div>
+  )
+}
+
+export default Nav
