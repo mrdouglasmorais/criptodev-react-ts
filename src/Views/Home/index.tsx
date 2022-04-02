5ca2cd4c04927a5002dc3168eb01aa890c7a5c50
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-04-02 11:55:18 -0300

    update styles

--- src/Views/Home/index.tsx
+++ src/Views/Home/index.tsx
@@ -3,39 +3,54 @@ import React, {useState, useEffect} from 'react';
 import Nav from '../../Components/Nav';
 import Footer from '../../Components/Footer';
 
-import Constants from '../../Constants'
-
 import { apiData } from '../../services/apiData';
 
 import {IStore} from '../../interfaces';
 
+import styles from './Home.module.css'
+
 const Home: React.FC = () => {
 
   const [data, setData] = useState<IStore[]>([]);
+  const [isLoad, setIsload] = useState<boolean>(false);
 
   useEffect(() => {
+    setIsload(true)
     apiData.get('stores').then(
       res => {
         setData(res.data)
       }
-    )
+    ).finally( () => setIsload(false))
   }, [])
 
   return(
     <>
       <Nav />
-      <h1>Listando Lojas</h1>
-      {data.map( (el, index) => (
-        <>
-          <div key={index}>
-            <h2>{el.label}</h2>
-            <img src={el.logo} alt={el.label} width={300} height={'auto'}/>
-            <p>{el.address.street}</p>
-            <p>{el.address.city}</p>
-            <p>{el.address.state}</p>
+      <div className={styles.container}>
+        <h1>Listando Lojas</h1>
+        { isLoad ? (
+          <>
+          <div>
+            <h3>Aguarde carregando</h3>
           </div>
-        </>
-      ))}
+          </>
+        ) : (
+          <>
+          {data.map( (el, index) => (
+            <div className={styles.storeLists}>
+              <div key={index}>
+                <h2>{el.label}</h2>
+                <img src={el.logo} alt={el.label} width={300} height={'auto'}/>
+                <p>{el.address.street}</p>
+                <p>{el.address.city}</p>
+                <p>{el.address.state}</p>
+              </div>
+            </div>
+          ))}
+          </>
+        )}
+      </div>
+      
       <Footer />
     </>
   )
